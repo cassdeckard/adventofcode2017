e571b0d0a974ff76a1edb7e3c7ca3aa8a59cab43
Matt Deckard <histumness@gmail.com>
2017-12-10 10:40:10 -0600

    Start of 10-2

--- AdventOfCode2017-10.swift
+++ AdventOfCode2017-10.swift
@@ -1,31 +1,39 @@
-let example = [3, 4, 1, 5]
-let input = [230,1,2,221,97,252,168,169,57,99,0,254,181,255,235,167]
+let input = "230,1,2,221,97,252,168,169,57,99,0,254,181,255,235,167"
 
 func process(_ input: [Int], listLength: Int = 256) -> [Int] {
 	var list = (0..<listLength).map{ $0 }
-	print(list)
+// 	print(list)
 
 	var unshift = 0
-	for (skip, length) in input.enumerated() {
-		print("length: \(length)  skip: \(skip)")
-		let subSequence = list[..<length]
-		list = subSequence.reversed() + list[length...]
-		let shift = (length + skip) % list.count
-		unshift += shift
-		list = list[shift...].map { $0 } + list[..<shift]
-		print(list)
+	var skip = 0
+	for i in 1...64 {
+		print(i)
+		for length in input {
+			print("length: \(length)  skip: \(skip)")
+			let subSequence = list[..<length]
+			list = subSequence.reversed() + list[length...]
+			let shift = (length + skip) % list.count
+			unshift += shift
+			list = list[shift...].map { $0 } + list[..<shift]
+			skip += 1
+		}
 	}
 
 	unshift = list.count - (unshift % list.count)
-	print(unshift)
+// 	print(unshift)
 
 	list = list[unshift...].map { $0 } + list[..<unshift]
 
-	print(list)
+// 	print(list)
 	return list
 }
 
-print(process(example, listLength: 5))
-print("###")
-let result = process(input)
-print(result[0] * result[1])
+let sparse = process([17, 31, 73, 47, 23])
+var dense = [Int]()
+for i in stride(from: 0, to: 256, by: 16) {
+	dense.append(sparse[i..<i+16].reduce(0, ^))
+}
+print(dense)
+//print(dense.map { String.init(format: "%02x", $0) })
+
+// print(String(format: "%02x", 15))
