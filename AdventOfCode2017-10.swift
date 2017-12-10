4addc13a895a78d2e87b5324bbeed2b319a4f7cc
Matt Deckard <histumness@gmail.com>
2017-12-10 10:52:33 -0600

    10-2 complete

--- AdventOfCode2017-10.swift
+++ AdventOfCode2017-10.swift
@@ -2,14 +2,11 @@ let input = "230,1,2,221,97,252,168,169,57,99,0,254,181,255,235,167"
 
 func process(_ input: [Int], listLength: Int = 256) -> [Int] {
 	var list = (0..<listLength).map{ $0 }
-// 	print(list)
 
 	var unshift = 0
 	var skip = 0
-	for i in 1...64 {
-		print(i)
+	for _ in 1...64 {
 		for length in input {
-			print("length: \(length)  skip: \(skip)")
 			let subSequence = list[..<length]
 			list = subSequence.reversed() + list[length...]
 			let shift = (length + skip) % list.count
@@ -20,27 +17,35 @@ func process(_ input: [Int], listLength: Int = 256) -> [Int] {
 	}
 
 	unshift = list.count - (unshift % list.count)
-// 	print(unshift)
 
 	list = list[unshift...].map { $0 } + list[..<unshift]
 
-// 	print(list)
 	return list
 }
 
-let sparse = process([17, 31, 73, 47, 23])
-var dense = [Int]()
-for i in stride(from: 0, to: 256, by: 16) {
-	dense.append(sparse[i..<i+16].reduce(0, ^))
-}
-print(dense)
+func hash(_ input: String) -> String {
+	let inputList = input.unicodeScalars.map { Int(UInt8(ascii: $0)) } + [17, 31, 73, 47, 23]
+	print(inputList)
 
-func toHex(_ int: Int) -> String {
-	let key = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
-	return key[(int / 16)] + key[(int % 16)]
+	let sparse = process(inputList)
+	var dense = [Int]()
+	for i in stride(from: 0, to: 256, by: 16) {
+		dense.append(sparse[i..<i+16].reduce(0, ^))
+	}
+	print(dense)
+
+	func toHex(_ int: Int) -> String {
+		let key = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+		return key[(int / 16)] + key[(int % 16)]
+	}
+
+	let denseStr = dense.reduce("") {
+		acc, next in acc + toHex(next)
+	}
+	return denseStr
 }
 
-let denseStr = dense.reduce("") {
-	acc, next in acc + toHex(next)
-}
-print(denseStr)
+print(hash("AoC 2017"))
+print(hash("1,2,3"))
+print(hash("1,2,4"))
+print(hash(input))
