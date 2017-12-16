e12cdbb8c508e05badfe76bbf89b8584a36e375a
Matt Deckard <histumness@gmail.com>
2017-12-16 09:00:49 -0600

    16-1 example

---
+++ AdventOfCode2017-16.py
@@ -0,0 +1,35 @@
+programs = [chr(i) for i in range(ord('a'), ord('f'))]
+
+def spin(programs, idx):
+  print(idx)
+  idx = int(idx)
+  slice_idx = len(programs) - idx
+  return programs[slice_idx:] + programs [:slice_idx]
+
+def exchange(programs, i, j):
+  (i, j) = (int(i), int(j))
+  temp = programs[i]
+  programs[i] = programs[j]
+  programs[j] = temp
+  return programs
+
+def partner(programs,a,b):
+  idx_a = programs.index(a)
+  idx_b = programs.index(b)
+  programs[idx_a] = b
+  programs[idx_b] = a
+  return programs
+
+move_map = { 's' : spin, 'x' : exchange, 'p' : partner }
+
+moves = "s1,x3/4,pe/b"
+moves = moves.split(',')
+moves = [(m[0], m[1:]) for m in moves]
+moves = [(m[0], m[1].split('/')) for m in moves]
+
+
+for move in moves:
+  print programs
+  programs = move_map[move[0]](programs, *move[1])
+
+print programs
