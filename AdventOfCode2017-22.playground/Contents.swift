51c39670ace8dfb1dbc53c23fd89caef9d71e2a2
dev1 <dev1@spr-md12.wwt.com>
2017-12-22 09:22:24 -0600

    22-1 : example

--- AdventOfCode2017-22.playground/Contents.swift
+++ AdventOfCode2017-22.playground/Contents.swift
@@ -8,7 +8,68 @@ let example = """
 ...
 """
 
-var map = example.asMap
+enum Direction {
+    case up, down, left, right
+    
+    var rotatedRight : Direction {
+        switch self {
+        case .up:
+            return .right
+        case .down:
+            return .left
+        case .left:
+            return .up
+        case .right:
+            return .down
+        }
+    }
+    
+    var rotatedLeft : Direction {
+        switch self {
+        case .up:
+            return .left
+        case .down:
+            return .right
+        case .left:
+            return .down
+        case .right:
+            return .up
+        }
+    }
+}
 
-//map["(x: -7, y: -5)"] = (x: -7, y: -5)
+func move(_ position: Coordinates, _ direction: Direction) -> Coordinates {
+    switch direction {
+    case .up:
+        return (x: position.x, y: position.y - 1)
+    case .down:
+        return (x: position.x, y: position.y + 1)
+    case .left:
+        return (x: position.x - 1, y: position.y)
+    case .right:
+        return (x: position.x + 1, y: position.y)
+    }
+}
+
+struct Virus {
+    var position = Coordinates(x: 0, y: 0)
+    var direction = Direction.up
+    
+    mutating func act(on map: inout [String : Coordinates]) {
+        if map.isInfected(node: self.position) {
+            self.direction = self.direction.rotatedRight
+        } else {
+            self.direction = self.direction.rotatedLeft
+        }
+        map.toggle(node: self.position)
+        self.position = move(self.position, self.direction)
+    }
+}
+
+var map = example.asMap
+var virus = Virus()
+
+for _ in 1...70 {
+    virus.act(on: &map)
+}
 print(map.grid)
--- AdventOfCode2017-22.playground/Sources/22.swift
+++ AdventOfCode2017-22.playground/Sources/22.swift
@@ -14,7 +14,7 @@ public func < (lhs: Coordinates, rhs: Coordinates) -> Bool {
 
 extension String {
     public var asMap : [String : Coordinates] {
-        var _map = self.split(separator: "\n")
+        let _map = self.split(separator: "\n")
             .map { Array($0) }
         
         var map = [String : Coordinates]()
@@ -42,13 +42,33 @@ extension Dictionary where Key == String, Value == Coordinates {
         return y ?? 0
     }
     
+    public func isInfected(node: Coordinates) -> Bool {
+        return self[String(describing: node)] != nil
+    }
+   
+    
+    public mutating func disinfect(node: Coordinates) {
+        self[String(describing: node)] = nil
+    }
+    
+    public mutating func infect(node: Coordinates) {
+        self[String(describing: node)] = node
+    }
+    
+    public mutating func toggle(node: Coordinates) {
+        if isInfected(node: node) {
+            disinfect(node: node)
+        } else {
+            infect(node: node)
+        }
+    }
+    
     public var grid : String {
         var result = ""
         for y in -maxY...maxY {
             for x in -maxX...maxX {
                 let pos = Coordinates(x: x, y: y)
-                let node = self[String(describing: pos)] == nil ? "." : "#"
-                result += "\(node) "
+                result += "\(self.isInfected(node: pos) ? "#" : ".") "
             }
             result += "\n"
         }
